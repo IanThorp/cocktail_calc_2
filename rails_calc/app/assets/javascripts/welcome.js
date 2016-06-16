904e2630980653e2f6a0e0baafbd937bf0d2a053
Ian Thorp <IanMThorp@gmail.com>
2016-06-16 12:32:43 -0700

    Add basic handlebars

---
+++ rails_calc/app/assets/javascripts/welcome.js
@@ -0,0 +1,46 @@
+(function() {
+
+	var ingredients = {
+		names: ['Campari', 'Averna'],
+		init: function(){
+			this.cacheDom();
+			this.bindEvents();
+			this.render();
+		},
+		cacheDom: function() {
+			this.$el = $('#ingredientModule');
+			this.$button = this.$el.find('button');
+			this.$input = this.$el.find('input');
+			this.$ul = this.$el.find('ul');
+			this.template = this.$el.find('#ingredient-template')
+		},
+		bindEvents: function() {
+			this.$button.on('click', this.addIngredient.bind(this));
+			this.$ul.delegate('i.del', 'click', this.deleteIngredient.bind(this))
+		},
+		render: function() {
+			var data = {
+				names: this.names
+			};
+			console.log(this.template);
+			console.log(template);
+			this.$ul.html(Handlebars.compile(this.template, data));
+		},
+		addIngredient: function() {
+			this.names.push(this.$input.val());
+			this.render();
+			this.$input.val('');
+		},
+		deleteIngredient: function(event) {
+			var $remove = $(event.target).closest('li');
+			var i = this.$ul.find('li').index($remove);
+
+			this.names.splice(i, 1);
+			this.render();
+		}
+
+	};
+
+	ingredients.init();
+
+}())
